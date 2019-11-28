11346fcfa4c4efd9066601e0fe2b78579baf8a10
CAROL BASTOS <CAROL.BASTOS@OCEANBRASIL.COM>
2019-11-28 12:05:00 -0400

    initial

---
+++ cmake-build-debug/CMakeFiles/mochila.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mochila.dir/main.cpp.obj"
+  "libmochila.dll.a"
+  "mochila.exe"
+  "mochila.exe.manifest"
+  "mochila.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mochila.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
