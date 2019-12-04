fe49e7e9e1c272f6844ecc292d7db1275ed07664
CAROL BASTOS <CAROL.BASTOS@OCEANBRASIL.COM>
2019-12-04 15:49:47 -0400

    pd problema da mochila

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
-#include <iostream>
-#include <vector>
-#define int long long
-using namespace std;
+// A Dynamic Programming based solution for 0-1 Knapsack problem
+#include<stdio.h>
 
-signed main(){
-    int n, frutos, k;
-    vector<int> loopapoons, pd;
-    cin >> n;
-    for(int i=0;i<n;i++){
-        cin >> frutos;
-        loopapoons.push_back(frutos);
-    }
-    cin >> k;
-    for(int i=0;i<n;i++){
-        if(i==0) pd.push_back(loopapoons[0]);
-        else if(i<k) pd.push_back(max(loopapoons[i],pd[i-1]));
-        else{
-            pd.push_back(max(loopapoons[i]+pd[i-k],pd[i-1]));
+// A utility function that returns maximum of two integers
+int max(int a, int b) { return (a > b)? a : b; }
+
+// Returns the maximum value that can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+    int K[n+1][W+1];
+
+    // Build table K[][] in bottom up manner
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= W; w++)
+        {
+            if (i==0 || w==0)
+                K[i][w] = 0;
+            else if (wt[i-1] <= w)
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+            else
+                K[i][w] = K[i-1][w];
         }
     }
-    cout << pd[n-1] << endl;
+
+    return K[n][W];
+}
+
+int main()
+{
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int  W = 50;
+    int n = sizeof(val)/sizeof(val[0]);
+    printf("%d", knapSack(W, wt, val, n));
     return 0;
 }
